d0f08d29847df21b19a99d0366275d4a0161c629
salamaa21-meet <salamaa21@meet.mit.edu>
2019-07-29 03:27:36 -0400

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,26 @@
+#1
+"""
+total=0
+for number in range(1, 10 +1):
+    print(number)
+    total=total+number
+print(total)
+"""
+#2
+"""def add_numbers():
+    total=0
+    for number in range(1, 10 +1):
+        print(number)
+        total=total+number
+    return(total)
+answer=add_numbers()
+print(answer)
+"""
+#3
+def salam(start,end):
+    test=0
+    for i in range(start,end+1):
+        test+=i
+    return(test)
+answer= salam(1,10)
+print(answer)
